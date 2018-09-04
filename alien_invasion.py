8321dbcdcf445f5723d893f176d97e5ef018046e
qwqqq6 <ballonsgoo@gmail.com>
2018-09-04 17:48:29 +0800

    1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
-import sys
 import pygame
 
 from setting import Settings
 from ship import  Ship
+import game_functions as gf
 
 def run_game():
     # 初始化
@@ -11,23 +11,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 开始游戏主循环
     while True:
-
-        # 监视键盘和鼠标
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        #重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # 最近绘制屏幕可见
-        pygame.display.flip()
-
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 if __name__ == "__main__":
     run_game()
--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #加载飞船图像
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,22 @@ class Ship():
         #确定飞船位置
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        #在飞船center属性中存储小数
+        self.center = float(self.rect.centerx)
+
+        #移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船的位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
 
     def blitme(self):
         """在指定位置绘制飞船"""
